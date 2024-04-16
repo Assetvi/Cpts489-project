bcf73e33c79b4def277f0f2ba1266615b2985964
fruitslayer1 <47836668+fruitslayer1@users.noreply.github.com>
2024-04-16 14:37:29 -0700

    completed logout functionality, prevented from accessing login/register pages while logged in

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -2,6 +2,17 @@ var express = require('express');
 const User = require('../models/User');
 var router = express.Router();
 
+const sessionChecker = (req, res, next)=>{
+    if(!req.session.user){
+    //   res.locals.username = req.session.user.username
+      next()
+    }else{
+      res.redirect("../?msg=raf")
+    }
+  }
+  
+  router.use(sessionChecker)
+
 // Route to render login
 router.get('/', function(req, res,next) {
     // Render login
@@ -22,14 +33,6 @@ router.post('/login', async function(req, res, next) {
     }
   });
   
-  router.get('/logout', function(req,res, next){
-    if(req.session.user){
-      req.session.destroy()
-      res.redirect("/?msg=logout")
-    }else {
-      res.redirect("/")
-    }
-    
-  })
+  
 
 module.exports = router;
